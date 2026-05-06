746f871cd3ea09624f87091198e2af4b27a310d5
maytawinsheet-byte <maytawin.sheet@gmail.com>
2026-05-06 09:42:57 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
-console.log('SYSTEM READY');
+const menuToggle = document.getElementById('menuToggle');
+const mobileMenu = document.getElementById('mobileMenu');
+
+menuToggle.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+});
