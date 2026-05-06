8dbe74d2c043c1abfb5d0c0282ea52fe8f37a24e
maytawinsheet-byte <maytawin.sheet@gmail.com>
2026-05-06 09:58:09 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,10 @@ const mobileMenu = document.getElementById('mobileMenu');
 menuToggle.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
 });
+const bookingBtn = document.getElementById('bookingBtn');
+
+bookingBtn.addEventListener('click', () => {
+
+  alert('ระบบจองโต๊ะกำลังจะเริ่มทำงาน');
+
+});
