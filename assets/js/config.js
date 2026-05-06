99573ed51ef7b9738a9c0f06f305df112ed73bb7
maytawinsheet-byte <maytawin.sheet@gmail.com>
2026-05-06 09:55:06 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 const SYSTEM_CONFIG = {
   appName: 'Hospitality Booking System',
   version: 'Production v2',
-  apiUrl: '',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwakbZ-3m3OQ5yILLzQoUxm01jZeIWtfRXduSEv96pspp_DXd398s_-zWEvzhdGJ1QV/exec',
   storeId: 'STORE001',
   defaultLanguage: 'th'
 };
